392051fdcac428680840b3278780d30e74190a8e
sujo028 <48432120+sujo028@users.noreply.github.com>
2019-10-24 22:49:32 +0530

    Add.c
    To find sum of two numbers

--- Add.java
+++ Add.java
@@ -1,14 +1,7 @@
-import java.util.Scanner;
-
-public class Add{
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int a,b;
-        System.out.print("Enter 1st numbers:");
-        a=input.nextInt();
-        System.out.print("Enter 2nd numbers:");
-        b=input.nextInt();
-        System.out.println("Sum="+(a+b));
-
-    }
+#include<stdio.h>
+void main(){
+ int a,b;
+ printf("Enter the number");
+ scanf("%d%d",&a,&b);
+ printf("Sum=%d",a+b);
 }
